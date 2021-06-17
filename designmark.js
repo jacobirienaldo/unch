1230da20d33d7e75626f8b11efc2541dd17575c0
jacobirienaldo <86066780+jacobirienaldo@users.noreply.github.com>
2021-06-17 22:09:18 +0700

    Add files via upload

---
+++ designmark.js
@@ -0,0 +1,10 @@
+function show()
+{
+document.getElementById('marktext').style.transform = "scale(5)";
+document.getElementById('marktext').style.opacity = "1";
+}
+function unshow()
+{
+document.getElementById('marktext').style.transform = "scale(1)";
+document.getElementById('marktext').style.opacity = "0";
+}
